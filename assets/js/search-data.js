fc3e3d758c249a81ea27e3d8175c9ad7d83465f3
lucienvidagbandji <144815737+lucienvidagbandji@users.noreply.github.com>
2025-07-20 18:57:29 +0000

    Deploying to gh-pages from @ lucienvidagbandji/lucienvidagbandji.github.io@7c3ccd849b897071c946b744fbb3effb9ae26b91 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,8 +9,8 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-talk",
-          title: "Talk",
+  },{id: "nav-blog",
+          title: "Blog",
           description: "",
           section: "Navigation",
           handler: () => {
